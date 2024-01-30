8d10c7e06f176540d06e2e748c7a2e8bf2491fb5
denisprinchanu <127018735+denisprinchanu@users.noreply.github.com>
2024-01-29 21:45:35 -0500

    homework

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 
 
-'use strict'
+
 
 
 
@@ -16,13 +16,8 @@ function getElementWidth(content, padding, border) {
 }
 
 
-
-
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
 
 
-
-// let content = "50px";
-// let numerical = Number.parseFloat(content);
